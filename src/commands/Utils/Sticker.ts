428dc71ca37a8f5a5ea54d6c45f96ac2824f327d
Kazuken <87819794+Kazuken2@users.noreply.github.com>
2021-08-06 01:06:55 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -28,8 +28,8 @@ export default class Command extends BaseCommand {
         const pack = parsedArgs.joined.split('|')
         const crop:boolean = parsedArgs.flags.includes('--stretch') || parsedArgs.flags.includes('--s')
         const sticker = new Sticker(buffer, {
-            pack: pack[1] || '👾 𝐇𝐚𝐧𝐝𝐜𝐫𝐚𝐟𝐭𝐞𝐝 𝐅𝐨𝐫 𝐘𝐨𝐮 ',
-            author: pack[2] || '𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭𝐭𝐨 𝐊𝐚𝐨𝐢 👾',
+            pack: pack[1] || '🧛‍♂️ 𝐇𝐚𝐧𝐝𝐜𝐫𝐚𝐟𝐭𝐞𝐝 𝐅𝐨𝐫 𝐘𝐨𝐮 ',
+            author: pack[2] || '*꧁༒Kazuken༒꧂*🧛‍♂️',
             crop: crop
         })
         await sticker.build()

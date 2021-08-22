53abd9f77ac4b888a4f4986d305388881a7b498c
Kazuken <87819794+Kazuken2@users.noreply.github.com>
2021-08-22 14:17:18 +0530

    Create quote.ts

---
+++ src/commands/Fun/quote.ts
@@ -0,0 +1,50 @@
+import MessageHandler from '../Handlers/MessageHandler'
+
+import BaseCommand from '../lib/BaseCommand'
+
+import WAClient from '../lib/WAClient'
+
+import { IParsedArgs, ISimplifiedMessage } from '../typings'
+
+export default class Command extends BaseCommand {
+
+    constructor(client: WAClient, handler: MessageHandler) {
+
+        super(client, handler, {
+
+            command: 'quote',
+
+            description: 'command description',
+
+            category: 'Fun',
+
+            usage: `${client.config.prefix}quote`
+
+        })
+
+    }
+
+    //eslint-disable-next-line
+
+    run = async (M: ISimplifiedMessage, args: IParsedArgs): Promise<void> => {
+  
+
+	const url = `https://api.quotable.io/random`;
+
+	try {
+
+		const response = await got(url);
+
+		const json = JSON.parse(response.body);
+
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📌 ' + Lang.QUOTE +'* ```' + json.content + '```\n\n' +
+
+		'*✒️' + Lang.AUTHOR +'* ```' + json.author+ '```\n', MessageType.text);
+
+	} catch {
+
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDA, MessageType.text);
+
+	}}
+
+}
